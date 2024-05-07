6005adba006488a5b428dc58cf479e9916f6c036
ankit <ankitbisht0205@gmail.com>
2024-05-07 12:18:28 +0530

    update vercel-build cmd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import "./loadEnvironment.mjs";
+import "./loadEnvironment.js";
 import "express-async-errors";
-import savePdf from "./routes/save-pdf.mjs";
+import savePdf from "./routes/save-pdf.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
